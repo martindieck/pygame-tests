13c2e59e5e74e161659cd308370c6cf9267a8212
martindieck <krutterlax@gmail.com>
2023-08-11 18:52:40 -0600

    Added Basic Ninja Game Shell

---
+++ NINJA_GAME/game.py
@@ -0,0 +1,26 @@
+import sys
+import pygame
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        SCREEN_WIDTH = 800
+        SCREEN_HEIGHT = 600
+
+        pygame.display.set_caption('Ninja Game')
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
