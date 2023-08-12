72678d4e3adfa87f68f08ddc6131fdbfb829fd80
martindieck <krutterlax@gmail.com>
2023-08-12 12:36:14 -0600

    Added utils and scripts and implemented OOP

--- NINJA_GAME/game.py
+++ NINJA_GAME/game.py
@@ -1,55 +1,54 @@
 import sys
 import pygame
+from scripts.entities import PhyisicsEntity
+from scripts.utils import load_image
 
 class Game:
     def __init__(self):
         pygame.init()
 
-        SCREEN_WIDTH = 800
-        SCREEN_HEIGHT = 600
+        SCREEN_WIDTH = 640
+        SCREEN_HEIGHT = 480
 
-        pygame.display.set_caption('Ninja Game')
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+        pygame.display.set_caption('Ninja Game')
+        pygame.display.set_icon(load_image('shuriken-icon.png'))
+        self.display = pygame.Surface((320, 240))
         self.clock = pygame.time.Clock()
 
-        self.img = pygame.image.load('NINJA_GAME\data\images\clouds\cloud_1.png')
-        self.img.set_colorkey((0, 0, 0))
-        
-        self.img_pos = [160, 260]
         self.movement = [False, False]
+
+        self.assets = {
+            'player': load_image('entities/player.png') 
+        }
 
         self.collision_area = pygame.Rect(50, 50, 300, 50)
 
+        self.player = PhyisicsEntity(self, 'player', (50, 50), (8, 15))
+
     def run(self):
         while True:
-            self.screen.fill((135, 206, 235))
+            self.display.fill((135, 206, 235))
             
-            # Look into squash operator!
-            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
-            if img_r.colliderect(self.collision_area):
-                pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
-            else:
-                pygame.draw.rect(self.screen, (0, 50, 255), self.collision_area)
-
-            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
-            self.screen.blit(self.img, self.img_pos)
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_LEFT:
                         self.movement[0] = True
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_LEFT:
                         self.movement[0] = False
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
 
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
             self.clock.tick(60)
 
