ed3760f6d3e90dbb497ae9b4fc549cabf8516103
martindieck <krutterlax@gmail.com>
2023-08-13 10:59:56 -0600

    Added Clouds

--- NINJA_GAME/game.py
+++ NINJA_GAME/game.py
@@ -4,6 +4,7 @@ import pygame
 from scripts.entities import PhysicsEntity
 from scripts.utils import load_image, load_images
 from scripts.tilemap import Tilemap
+from scripts.clouds import Clouds
 
 class Game:
     def __init__(self):
@@ -26,8 +27,11 @@ class Game:
             'stone': load_images('tiles/stone'),
             'large_decor': load_images('tiles/large_decor'),
             'player': load_image('entities/player.png'),
-            'background': load_image('background.png')
+            'background': load_image('background.png'),
+            'clouds': load_images('clouds')
         }
+
+        self.clouds = Clouds(self.assets['clouds'], count = 16)
 
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
 
@@ -47,6 +51,9 @@ class Game:
             self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 15
             self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 15
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.clouds.update()
+            self.clouds.render(self.display, offset=render_scroll)
 
             self.tilemap.render(self.display, offset = render_scroll)
 
